2fb60251c526a397fa5646f281e18121a616537c
wadloup <lotfinobl.2005@hotmail.com>
2026-03-29 12:48:58 +0200

    Update IconRow.tsx

--- components/ui/IconRow.tsx
+++ components/ui/IconRow.tsx
@@ -1,19 +1,18 @@
 type IconRowProps = {
   icons: string[];
+  onError?: (event: React.SyntheticEvent<HTMLImageElement>) => void;
 };
 
-export default function IconRow({ icons }: IconRowProps) {
+export default function IconRow({ icons, onError }: IconRowProps) {
   return (
-    <div className="flex flex-wrap gap-2">
+    <div className="flex flex-wrap gap-3">
       {icons.map((src, index) => (
         <img
           key={`${src}-${index}`}
           src={src}
-          alt={`rune icon ${index + 1}`}
-          className="h-11 w-11 shrink-0 rounded-lg border border-red-500/30 bg-black/50 p-1 object-contain"
-          onError={(event) => {
-            event.currentTarget.style.display = "none";
-          }}
+          alt="rune icon"
+          className="h-14 w-14 rounded-xl border border-red-500/30 bg-black/50 object-cover"
+          onError={onError}
         />
       ))}
     </div>
